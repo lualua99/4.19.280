be52aa866ab269c4dad0072b38e84e2d90eced58
lualua99 <lualua99@users.noreply.github.com>
2023-05-19 23:32:56 +0800

    “4.19.280”

---
+++ lmperf/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ARCH=arm64
+export SUBARCH=arm64
+export DTC_EXT=/usr/bin/dtc
+export KBUILD_BUILD_USER=lmperf
+export KBUILD_BUILD_HOST=localhost
+export KERNEL_ROOTDIR=`pwd`
+export KERNEL_OUTDIR=${KERNEL_ROOTDIR}/out
+target=alioth_lmperf_defconfig
+
+echo "Start building..."
+cd ${KERNEL_ROOTDIR}
+make LLVM=1 LLVM_IAS=1 O=${KERNEL_OUTDIR} ARCH=${ARCH} SUBARCH=${SUBARCH} CC=clang LD=ld.lld ${target}
+cd ${KERNEL_OUTDIR}
+make -j$(nproc) LLVM=1 LLVM_IAS=1 O=${KERNEL_OUTDIR} ARCH=${ARCH} SUBARCH=${SUBARCH} CC=clang LD=ld.lld CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- KBUILD_BUILD_USER=${KBUILD_BUILD_USER} KBUILD_BUILD_HOST=${KBUILD_BUILD_HOST}
+
---
+++ lmperf/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# KernelSU
+git clone https://github.com/tiann/KernelSU.git
+# lmperf
+cd lmperf/
+git clone https://github.com/LeviMarvin/AnyKernel3.git
+mkdir out
+
